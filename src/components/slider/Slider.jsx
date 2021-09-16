0314a0e3bf0e4d179ac503a4c2b2b5045b7c14bc
nguyenvanluat20 <70328167+nguyenvanluat20@users.noreply.github.com>
2021-09-16 16:42:19 +0700

    hello.jsx
    Ok

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,7 +1,33 @@
 import "./slider.css"
 import Carousel from "react-elastic-carousel";
 import Card from "../card/Card";
+import axios from "axios";
+const axiosInstance = axios.create({
+    baseURL: "https://apiwebmovie.herokuapp.com",
+});
+export default axiosInstance;
+const [values, setValues] = useState({
+    moviename: '',
+    trailerlink: '',
+    movielink: '',
+    imagelink: '',
+    image: '',
+    typemovie: '',
+    national: '',
+    actors: '',
+    description: ''
+});
+
 export default function Slider() {
+    useEffect(() => {
+        const fetchMovie = async () => {
+            const response = await axios.get("/getallmovie");
+            const data = await response.data;
+            setValues(data);
+            console.log(data);
+        };
+        fetchMovie();
+    }, [id]);
     const breakPoints = [
         { width: 1, itemsToShow: 1 },
         { width: 500, itemsToShow: 2 },
